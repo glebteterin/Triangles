63bb216f333cec601e2775dc4a34f2e069d35036
Gleb Teterin <to@glebteterin.com>
2012-01-16 10:15:21 +0600

    DAL: Receipts repositpry has been added

---
+++ src/Code/DataAccess/ReceiptRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace Triangles.Code.DataAccess
+{
+	public class ReceiptRepository
+	{
+		public void Insert(Receipt receipt)
+		{
+			using (var context = new TrianglesDataContext())
+			{
+				context.Receipts.InsertOnSubmit(receipt);
+				context.SubmitChanges();
+			}
+		}
+
+		public void Update(Receipt receipt)
+		{
+			using (var context = new TrianglesDataContext())
+			{
+				var existedReceipt = context.Receipts.First(x => x.Id == receipt.Id);
+
+				existedReceipt.Description = receipt.Description;
+				existedReceipt.Payer = receipt.Payer;
+
+				context.SubmitChanges();
+			}
+		}
+
+		public void Delete(int id)
+		{
+			using (var context = new TrianglesDataContext())
+			{
+				var receiptToDelete = context.Receipts.First(x => x.Id == id);
+				context.Receipts.DeleteOnSubmit(receiptToDelete);
+				context.SubmitChanges();
+			}
+		}
+
+		public Receipt[] BySessionUrl(string sessionUrl)
+		{
+			using (var context = new TrianglesDataContext())
+			{
+				return context.Receipts.Where(x => x.Session.UniqueUrl == sessionUrl).ToArray();
+			}
+		}
+	}
+}
